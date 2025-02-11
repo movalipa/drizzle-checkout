ed083d7aa57dba856575d5ef93de430c9ed4876b
movalipa <movalipa@gmail.com>
2025-02-11 15:57:26 +0330

    update checked

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { usersTable } from "./db/schema.js";
+import { eq, ne, sql } from "drizzle-orm";
 
 const db = drizzle(process.env.DATABASE_URL!);
 
 async function main() {
-  const res = await db
-    .insert(usersTable)
-    .values({ name: "me", age: 20, email: "sa" })
-    .catch((s) => s);
-  console.log("new user", res);
+  //   const res = await db
+  //     .insert(usersTable)
+  //     .values({ name: "me", age: 20, email: "dsdss" });
+  await db.update(usersTable).set({ age: 0 }).where(eq(usersTable.email, "sa"));
+  const res = await db.select().from(usersTable);
+  console.log(res);
 }
 
 main();
